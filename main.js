b8d217d216020f73140980fec49d941da5be1f81
amey-cuelogic <amey.kalekar@cuelogic.com>
2019-06-10 19:08:07 +0530

    added js for basics

--- main.js
+++ main.js
@@ -1,1 +1,54 @@
-alert("Connected");
+// console.log("Connected");
+// console.log( 2 - '4' );
+// console.log( '2' - 5 );
+// console.log( '2' - '7' );
+// console.log( '6' / '2' );
+
+
+// let x = 'abc' + ' ' + 'xyz';
+
+// console.log(x);
+
+// let apples = "2";
+// let oranges = "3";
+// console.log( apples + oranges );
+
+// let apples = 2;
+// let oranges = 3;
+// console.log( apples + oranges );
+
+// let y = 2 * 2 + 12;// (2*2 =4)(4 +12)
+// console.log(y); //16
+
+// let m = n = o = 2 * 9 / 3 - 12 + 15;
+// console.log(m);
+// console.log(n);
+// console.log(o); // 2 * 9 = 18 / 3 = 6 // 6 -12 = -6 // -6 + 15 = 9
+
+// let a = 1;
+// let b = 2;
+
+// let c = 5 - (a = ++b);
+
+// console.log( a ); // 3
+// console.log( c ); // 0
+
+// console.log( 2 ** 2 ); //(2 * 2)
+// console.log( 2 ** 3 ); //(2 * 2 * 2)
+// console.log( 2 ** 4 ); //(2 * 2 * 2 * 2)
+
+
+
+let counter_Increment = 1;
+counter_Increment++;
+console.log(counter_Increment)
+
+
+let c = ++counter_Increment;
+
+
+console.log(c);
+
+
+
+
